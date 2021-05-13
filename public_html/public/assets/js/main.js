769d5ee3521ff3cc3fb5753cf929d605a3239fd4
Garry Macphee <garrymacphee@hotmail.com>
2021-05-13 19:22:24 +0100

    Update main.js

--- public_html/public/assets/js/main.js
+++ public_html/public/assets/js/main.js
@@ -1,10 +1,8 @@
 var chat = {};
 
-//get all messages for this chat
 chat.fetchMessages = function ()
     {
         var key1 = $('#form_key').val();
-       // var message = $('#message').val();
 
         $.ajax({
             url: '/chats/fetch/',
@@ -65,8 +63,6 @@ function startChatConnection()
     }
 
 
-
-
 function updateChatMessage2()
     {
         var message = $('#message').val();
@@ -85,71 +81,9 @@ function updateChatMessage2()
         });
     }
 
-/*
- function message_submit()
- {
- //$("#messageId").submit();
- }
- ;
- 
- function message_clear_text()
- {
- //$("#list_search").val("");
- //manage_filter_list_submit();
- }
- 
- 
- function updateMessages()
- {
- alert("Javascript fired!!!!");
- 
- var chat = $('#messageId').val();
- var key1 = $('#form_key').val();
- // $.getJSON("/app/controllers/add/?jsoncallback=?",
- $.getJSON("/public/chats/add/?jsoncallback=?",
- {chat: chat, key1: key1},
- function (data)
- {
- console.log(data);
- if (data.r === 1)
- {
- $('header_sec').append(data.d);
- $('contact_sec').append(data);
- }
- else
- {
- //$('#messId').append("<p>Error</p>");
- // $("#modal-title").html(data.title);
- // $("#modal-body").html(data.html);
- }
- });
- }
- 
- 
- function updateChatMessage(message)
- {
- var chat = $('#messageId').val();
- var key1 = $('#form_key').val();
- 
- $.getJSON("/public/chats/add/?jsoncallback=?",
- {chat: chat, key1: key1},
- function (data)
- {
- if (data.r == 1)
- {
- 
- }
- });
- 
- }
- 
- 
- */
 
 function showModal(modalTitle, modalBody)
     {
         $("#modal-general-title").html(modalTitle);
         $("#modal-general-content").html(modalBody);
-        //$("#modal-general-buttons").html(data.button);
-        //$('#modal-general').modal().trigger('focus');
     }
