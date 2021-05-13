435d52777457d446ef21c2fd8cc7f0bc5e32eac6
Garry Macphee <garrymacphee@hotmail.com>
2021-05-13 19:16:33 +0100

    Update session_helper.php

--- public_html/app/helpers/session_helper.php
+++ public_html/app/helpers/session_helper.php
@@ -1,8 +1,5 @@
 <?php
-
-
 session_start();
- 
 
 /**
  * See if the user is logged in by checking
@@ -12,73 +9,58 @@ session_start();
  * is set, false otherwise
  */
 function isLoggedIn()
-    {
-        if (isset($_SESSION['user_id']))
-            {
-                return true;
-            }
-        else
-            {
-                return false;
-            }
+{
+    if (isset($_SESSION['user_id'])) {
+        return true;
+    } else {
+        return false;
     }
+}
 
-//Flash message helper
-//EXAMPLE - flash('register_success', 'You are now registered', 'alert alert-danger);
-//DISPLAY IN VIEW - <php echo flash('register_success'); >
 /**
  * We are using the session name as the key and the message as the value
  * After the message has been displayed we unset the session values
  */
 function flash($name = '', $message = '', $class = 'alert alert-success')
-    {
-        if (!empty($name))
-            {
-                if (!empty($message) && empty($_SESSION[$name]))
-                    {
-                        if (!empty($_SESSION[$name]))
-                            {
-                                unset($_SESSION[$name]);
-                            }
-
-                        if (!empty($_SESSION[$name . '_class']))
-                            {
-                                unset($_SESSION[$name . '_class']);
-                            }
-
-                            //set the session name if doesn't exist
-                            $_SESSION[$name] = $message;
-                            $_SESSION[$name . '_class'] = $class;
-                    }
-                elseif (empty($message) && !empty($_SESSION[$name]))
-                    {
-                        $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
-
-                        echo '<div class="' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
-
-                        unset($_SESSION[$name]);
-                        unset($_SESSION[$name . '_class']);
-                    }
+{
+    if (!empty($name)) {
+        if (!empty($message) && empty($_SESSION[$name])) {
+            if (!empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
             }
-    }
 
-    /**
-     * Remove a flash message from the screen
-     * before the next page redirect
-     * @param type $name ref of the message to remove
-     */
-    function cancelFlash($name, $message)
-        {
-            if (!empty($_SESSION[$name . '_class']))
-                {
-                    unset($_SESSION[$name . '_class']);
-                }
-            if (!empty($_SESSION[$name]))
-                {
-                    unset($_SESSION[$name]);
-                }
-            if (!empty($_SESSION[$message]))
-                {
-                    unset($_SESSION[$message]);
-                }
+            if (!empty($_SESSION[$name . '_class'])) {
+                unset($_SESSION[$name . '_class']);
+            }
+
+            //set the session name if doesn't exist
+            $_SESSION[$name] = $message;
+            $_SESSION[$name . '_class'] = $class;
+        } elseif (empty($message) && !empty($_SESSION[$name])) {
+            $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
+
+            echo '<div class="' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
+
+            unset($_SESSION[$name]);
+            unset($_SESSION[$name . '_class']);
         }
+    }
+}
+
+/**
+ * Remove a flash message from the screen
+ * before the next page redirect
+ * @param type $name ref of the message to remove
+ */
+function cancelFlash($name, $message)
+{
+    if (!empty($_SESSION[$name . '_class'])) {
+        unset($_SESSION[$name . '_class']);
+    }
+    if (!empty($_SESSION[$name])) {
+        unset($_SESSION[$name]);
+    }
+    if (!empty($_SESSION[$message])) {
+        unset($_SESSION[$message]);
+    }
+}
