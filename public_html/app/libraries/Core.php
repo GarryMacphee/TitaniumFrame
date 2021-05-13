ed76b94a96d6e4764643b4648e578a69b0fe85c5
Garry Macphee <garrymacphee@hotmail.com>
2021-05-13 19:13:32 +0100

    Update Core.php

--- public_html/app/libraries/Core.php
+++ public_html/app/libraries/Core.php
@@ -6,64 +6,52 @@
  * URL FORMAT- /controller/method/params
  */
 class Core
+{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct()
     {
-        protected $currentController = 'Pages';
-        protected $currentMethod     = 'index';
-        protected $params            = [];
+        $url = $this->getUrl();
 
-        public function __construct()
-            {
-                $url = $this->getUrl();
+        if ($url != null) {
+            if (file_exists('../app/controllers/' . ucwords($url[0] . '.php'))) {
 
-				if($url != null)
-					{
-
-						if (file_exists('../app/controllers/'.ucwords($url[0].'.php')))
-							{
-
-								$this->currentController = ucwords ($url[0]);
-								
-
-								unset ($url[0]);
-							}
-					}
-
-                require_once '../app/controllers/'.$this->currentController.'.php';
-
-
-                $this->currentController = new $this->currentController;
-
-
-                if (isset ($url[1]))
-                    {
-
-                        if (method_exists($this->currentController, $url[1]))
-                            {
-                                $this->currentMethod = $url[1];
-                                // unset it now we have the value
-                                unset ($url[1]);
-                            }
-                    }
-
-
-                $this->params = $url ? array_values ($url) : [];
-
-
-                call_user_func_array ([$this->currentController, $this->currentMethod], $this->params);
+                $this->currentController = ucwords($url[0]);
+                unset ($url[0]);
             }
+        }
 
-            /**
-             * @return type Get the URL from address bar
-             */
-            public function getUrl ()
-                {
-                    if (isset($_GET['url']))
-                        {
-                            $url = rtrim (filter_input (INPUT_GET, 'url'), '/');
-                            $url = filter_var ($url, FILTER_SANITIZE_URL);
-                            $url = explode ('/', $url);
+        require_once '../app/controllers/' . $this->currentController . '.php';
 
-                            return $url;
-                        }
-                }
+        $this->currentController = new $this->currentController;
+
+        if (isset ($url[1])) {
+
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                // unset it now we have the value
+                unset ($url[1]);
+            }
+        }
+
+
+        $this->params = $url ? array_values($url) : [];
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
+
+    /**
+     * @return type Get the URL from address bar
+     */
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim(filter_input(INPUT_GET, 'url'), '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
+    }
+}
